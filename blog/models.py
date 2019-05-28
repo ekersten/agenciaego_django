277a5a997a882c8a97b420d1230640926c057847
Eric Kersten <ekersten@gmail.com>
2019-05-28 18:09:31 -0300

    blog sin empleados ni links

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,5 @@
+import random
+
 from django.db import models
 from wagtail.core.models import Page
 from wagtail.core.fields import StreamField
@@ -25,6 +27,8 @@ class BlogPage(BasePage):
     parent_page_types = ['blog.BlogListingPage']
 
     is_use_case = models.BooleanField(default=False)
+    short_description = models.TextField(blank=True, null=True)
+    likes = models.PositiveIntegerField(default=random.randint(900, 1500))
 
     content = StreamField(
         [
@@ -35,9 +39,15 @@ class BlogPage(BasePage):
     )
 
     content_panels = BasePage.content_panels + [
+        FieldPanel('short_description'),
         FieldPanel('is_use_case'),
+        FieldPanel('likes'),
         StreamFieldPanel('content')
     ]
+
+    @property
+    def formatted_title(self):
+        return '<h1 class="header__title">{}</h1>'.format(self.title)
 
     class Meta:
         verbose_name = 'Blog Page'
