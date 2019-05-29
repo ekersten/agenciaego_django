95aced4018c349b3074bdf920f61f24e2871875d
Eric Kersten <ekersten@gmail.com>
2019-05-29 15:12:34 -0300

    home

--- home/blocks.py
+++ home/blocks.py
@@ -1,2 +1,24 @@
+from django.utils.translation import ugettext as _
 from wagtail.core import blocks
+from wagtail.admin.blocks import PageChooserBlock
 
+class PageBlock(blocks.StructBlock):
+    page = PageChooserBlock()
+    
+    class Meta:
+        template = 'home/blocks/page_block.html'
+        icon = 'site'
+        label = _('Featured Page')
+
+
+class ThreePagesBlock(blocks.StructBlock):
+    title = blocks.CharBlock(required=True, default='Publicaciones destacadas')
+
+    page1 = PageChooserBlock(label='Pagina 0001')
+    page2 = PageChooserBlock()
+    page3 = PageChooserBlock()
+
+    class Meta:
+        template = 'home/blocks/featured_pages.html'
+        icon = 'placeholder'
+        label = _('Featured Pages')
--- home/models.py
+++ home/models.py
@@ -1,7 +1,24 @@
 from django.db import models
 
 from wagtail.core.models import Page
+from wagtail.core.fields import StreamField
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+
+from .blocks import PageBlock, ThreePagesBlock
 
 
 class HomePage(Page):
     template = 'home/home.html'
+
+    content = StreamField(
+        [
+            ('page', PageBlock()),
+            ('three_pages', ThreePagesBlock()),
+        ],
+        null=True,
+        blank=True
+    )
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('content')
+    ]
