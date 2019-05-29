b0b79cdf265996dd703a039c8ea82e6fb9bf0c11
Eric Kersten <ekersten@gmail.com>
2019-05-29 17:01:06 -0300

    links en blog

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ import random
 from django.db import models
 from django import forms
 from django.utils.translation import ugettext as _
-from wagtail.core.models import Page
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import StreamField
 from wagtail.core.blocks import CharBlock
 from wagtail.admin.edit_handlers import (
@@ -13,7 +13,7 @@ from wagtail.admin.edit_handlers import (
     InlinePanel
 ) 
 
-from modelcluster.models import ParentalManyToManyField
+from modelcluster.models import ParentalManyToManyField, ParentalKey
 
 from core.models import BasePage
 
@@ -40,6 +40,30 @@ class BlogListingPage(Page):
     class Meta:
         verbose_name = 'Blog Listing Page'
         verbose_name_plural = 'Blog Listing Pages'
+
+class BlogPageLink(Orderable):
+    page = ParentalKey('blog.BlogPage', related_name='links')
+    url = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    target = models.CharField(
+        max_length=6,
+        choices=[
+            ('_self', _('Same window')),
+            ('_blank', _('New window')),
+        ],
+        default='_self'
+    )
+
+    panels = [
+        MultiFieldPanel(
+            [
+                FieldPanel('url'),
+                FieldPanel('title'),
+                FieldPanel('target'),
+            ]
+        )
+    ]
+
 
 
 class BlogPage(BasePage):
@@ -73,6 +97,12 @@ class BlogPage(BasePage):
         FieldPanel('short_description'),
         FieldPanel('is_use_case'),
         FieldPanel('likes'),
+        MultiFieldPanel(
+            [
+                InlinePanel('links', label=_('Link'))
+            ],
+            heading=_('Links')
+        ),
         MultiFieldPanel(
             [
                 FieldPanel('employees', widget=forms.SelectMultiple)
