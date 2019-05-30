6f90e9b6676e19a243045c3d466578dd21d9b20b
Eric Kersten <ekersten@gmail.com>
2019-05-30 15:05:05 -0300

    settings prod

--- agenciaego/settings/production.py
+++ agenciaego/settings/production.py
@@ -1,6 +1,12 @@
+import os
 from .base import *
 
 DEBUG = False
+
+MEDIA_ROOT = os.path.normpath(os.path.join(os.path.dirname(__file__), '../media/').replace('\\','/'))
+STATIC_ROOT = os.path.normpath(os.path.join(os.path.dirname(__file__), '../static/').replace('\\','/'))
+
+BASE_URL = 'http://wagtail.egoagencydev.com'
 
 try:
     from .local import *
