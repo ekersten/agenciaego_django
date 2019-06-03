a5f5bca1755d85508f9ce6f1382bcf4d11ad65c5
Eric Kersten <ekersten@gmail.com>
2019-06-03 11:14:35 -0300

    settings prod y datos

--- agenciaego/settings/production.py
+++ agenciaego/settings/production.py
@@ -3,6 +3,17 @@ from .base import *
 
 DEBUG = False
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'agenciaego_django',
+        'USER': 'wagtail',
+        'PASSWORD': 'Wagtail123!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
 MEDIA_ROOT = os.path.normpath(os.path.join(os.path.dirname(__file__), '../media/').replace('\\','/'))
 STATIC_ROOT = os.path.normpath(os.path.join(os.path.dirname(__file__), '../static/').replace('\\','/'))
 
