6d5d841312b150b36f342dc16a905832f8e8c637
Eric Kersten <ekersten@gmail.com>
2019-06-04 17:46:45 -0300

    wsgi prod

--- agenciaego/wsgi_prod.py
+++ agenciaego/wsgi_prod.py
@@ -8,37 +8,37 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
-import site
+# import sys
+# import site
 
 
-def execfile(filepath, globals=None, locals=None):
-    if globals is None:
-        globals = {}
-    globals.update({
-        "__file__": filepath,
-        "__name__": "__main__",
-    })
-    with open(filepath, 'rb') as file:
-        exec(compile(file.read(), filepath, 'exec'), globals, locals)
+# def execfile(filepath, globals=None, locals=None):
+#     if globals is None:
+#         globals = {}
+#     globals.update({
+#         "__file__": filepath,
+#         "__name__": "__main__",
+#     })
+#     with open(filepath, 'rb') as file:
+#         exec(compile(file.read(), filepath, 'exec'), globals, locals)
 
 
-site.addsitedir('/home/egoagency/webapps/wagtail/.virtualenv/lib/python3.7/site-packages')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'agenciaego.settings.production'
-activate_this = os.path.expanduser('/home/egoagency/webapps/wagtail/.virtualenv/bin/activate_this.py')
-execfile(activate_this, dict(__file__=activate_this))
+# site.addsitedir('/home/egoagency/webapps/wagtail/.virtualenv/lib/python3.7/site-packages')
+# os.environ['DJANGO_SETTINGS_MODULE'] = 'agenciaego.settings.production'
+# activate_this = os.path.expanduser('/home/egoagency/webapps/wagtail/.virtualenv/bin/activate_this.py')
+# execfile(activate_this, dict(__file__=activate_this))
 
-project = '/home/egoagency/webapps/wagtail/site'
-workspace = os.path.dirname(project)
-sys.path.append(workspace)
+# project = '/home/egoagency/webapps/wagtail/site'
+# workspace = os.path.dirname(project)
+# sys.path.append(workspace)
 
-sys.path = ['/home/egoagency/webapps/wagtail/site',
-            '/home/egoagency/webapps/wagtail/'] + sys.path
+# sys.path = ['/home/egoagency/webapps/wagtail/site',
+#             '/home/egoagency/webapps/wagtail/'] + sys.path
 
 
 
-from django.core.wsgi import get_wsgi_application
+# from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "agenciaego.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "agenciaego.settings.production")
 
 application = get_wsgi_application()
