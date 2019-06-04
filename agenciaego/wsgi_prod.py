925f8f2fc36141de81b2b4208ee19258fc507383
Eric Kersten <ekersten@gmail.com>
2019-06-04 17:53:46 -0300

    wsgi prod

--- agenciaego/wsgi_prod.py
+++ agenciaego/wsgi_prod.py
@@ -37,7 +37,7 @@ import os
 
 
 
-# from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "agenciaego.settings.production")
 
